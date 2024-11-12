e78533f0efa59b9f0afed1d973668874c08d2072
Josip Tomo Licardo <licardo.josip@gmail.com>
2024-11-12 11:50:26 +0100

    Update data

--- src/generate_dummy_data.py
+++ src/generate_dummy_data.py
@@ -1,5 +1,5 @@
 import polars as pl
-from datetime import date, timedelta
+from datetime import date, timedelta, time
 import random
 
 random.seed(42)
@@ -10,22 +10,21 @@ DAYS = 10
 
 dates = [(start_date + timedelta(days=i)).strftime("%Y-%m-%d") for i in range(DAYS)]
 
-time_slots = [f"{hour}:00-{hour+1}:00" for hour in range(8, 20)]  # 8 to 19
+time_slots = [(time(hour, 0), time(hour + 1, 0)) for hour in range(8, 20)]  # 8 to 19
 
 schedule = []
 for date in dates:
-    for time in time_slots:
+    for start_time, end_time in time_slots:
         hall_free = 1 if random.random() > 0.3 else 0
-
         room_free = 1 if random.random() > 0.5 else 0
-
         seats_taken = random.randint(0, 10)
         seats_free = TOTAL_SEATS - seats_taken
 
         schedule.append(
             {
                 "Date": date,
-                "Time Slot": time,
+                "Start Time": start_time,
+                "End Time": end_time,
                 "Conference Hall Available": hall_free,
                 "Meeting Room Available": room_free,
                 "Coworking Seats Available": seats_free,
@@ -33,7 +32,7 @@ for date in dates:
         )
 
 df = pl.DataFrame(schedule)
-df = df.sort(["Date", "Time Slot"])
+df = df.sort(["Date", "Start Time"])
 print(df)
 
 df.write_csv("hall_reservation_schedule.csv")
